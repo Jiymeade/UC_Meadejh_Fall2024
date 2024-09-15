b887b9a11a3d43a6223c2cc4c9494d3c2160bf81
Jiymeade <Jiymeade@gmail.com>
2024-09-15 07:16:08 -0400

    Revert "Final New Changes"
    This reverts commit 5ee99f3019298353af1935ef900643c1cf343990.

--- UC NetCode Fall 2024/Assets/App/Resource/Scripts/Player/playmovement.cs
+++ UC NetCode Fall 2024/Assets/App/Resource/Scripts/Player/playmovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Jobs;
 
 namespace App.Resource.Scripts.Player
 
@@ -11,13 +11,13 @@ namespace App.Resource.Scripts.Player
     public class playmovement : MonoBehaviour
     {
         // Start is called before the first frame update
-        public void Update()
+        public void UPdate()
         {
-            Vector3 moveDirection = new Vector3(x:0, y:0, z:0);
+            Vector3 moveDirection = new Vector3(x: 0, y: 0, z: 0);
             if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
             if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
-            if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
-            if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
+            if (Input.GetKey(KeyCode.A)) moveDirection.z = -1f;
+            if (Input.GetKey(KeyCode.D)) moveDirection.z = +1f;
 
             float moveSpeed = 3f;
             transform.position += moveDirection * (moveSpeed * Time.deltaTime);
