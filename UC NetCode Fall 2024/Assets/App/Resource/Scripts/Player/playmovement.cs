13a43e27536b47d473c237d3f6f367c14c3cbfe4
Jiymeade <Jiymeade@gmail.com>
2024-10-23 01:23:47 -0400

    Module 8
    -Create animator controller that has at least the following animations : Idle, Walking, Jumping, and Punching.
    
    -Sync them with using a server and ownership with the respective player prefabs
    -Create a pull request and document the recorder to show the animations syncing across the net.
    
    * Punch and Jump may have to be pressed multiple times in order to work, I don't know is the problem was my computer overheating or the coding

--- UC NetCode Fall 2024/Assets/App/Resource/Scripts/Player/playmovement.cs
+++ UC NetCode Fall 2024/Assets/App/Resource/Scripts/Player/playmovement.cs
@@ -11,8 +11,23 @@ namespace App.Resource.Scripts.Player
 
     public class playmovement : NetworkBehaviour
     {
+
+        [SerializeField] private Animator _myAnimator;
+        [SerializeField] private OwnerNetworkAnimator _ownerAnimator;
+
         // Start is called before the first frame update
-        public void Update()
+
+        private void Awake()
+        {
+            if (_myAnimator == null) { 
+                _myAnimator = gameObject.GetComponent<Animator>();
+            }
+            if (_ownerAnimator == null) { 
+                _ownerAnimator = gameObject.GetComponent<OwnerNetworkAnimator>(); 
+            }
+
+        }
+        private void FixedUpdate()
         {
             if (!IsOwner) return;
 
@@ -21,6 +36,22 @@ namespace App.Resource.Scripts.Player
             if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
             if (Input.GetKey(KeyCode.A)) moveDirection.x = -1f;
             if (Input.GetKey(KeyCode.D)) moveDirection.x = +1f;
+            if (Input.GetKey(KeyCode.Space)) _ownerAnimator.SetTrigger("JumpTrigger");
+            if (Input.GetKey(KeyCode.Z)) _ownerAnimator.SetTrigger("PunchTrigger");
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                _myAnimator.SetBool(name:"isSprinting", value: true);
+            }
+            else
+            {
+                _myAnimator.SetBool(name: "isSprinting", value: false);
+            }
+
+
+            _myAnimator.SetBool(name:"isWalking", value:moveDirection.z != 0 || moveDirection.x != 0);
+            
+            
+
 
             float moveSpeed = 3f;
             transform.position += moveDirection * (moveSpeed * Time.deltaTime);
